069ec3068a7eb0b8d1d5e6b850f025dad99c46ad
Ragav Sachdeva <ragdev707@gmail.com>
2018-02-08 14:27:06 +1030

    added screenshot functionality

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ y_max = max(plydata.elements[0].data['y'])
 z_min = min(plydata.elements[0].data['z'])
 z_max = max(plydata.elements[0].data['z'])
 
+#Tweak these parameters to adjust the initial camera position
 c_x = (x_min + x_max)/2
-c_y = (y_min + y_max)/2
-c_z = z_min - (z_max - z_min + max(x_max-x_min, y_max-y_min))
+c_z = (y_min + y_max)/2
+c_y = z_min - (z_max - z_min + max(x_max-x_min, y_max-y_min))
 
 l_x = (x_min + x_max)/2
 l_y = (y_min + y_max)/2
@@ -59,7 +60,7 @@ message = """
         #Logo {
             height: 100px;
         }
-        #screenshot {
+        #myBtn {
             position: absolute;            
             background-color: rgb(245, 139, 69);
             border: none;
@@ -71,12 +72,67 @@ message = """
             font-size: 12px;
             top: 0px;            
             left: 0px;            
-        }        
+        }
+
+                /* The Modal (background) */
+        .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            padding-top: 100px; /* Location of the box */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+
+        /* Modal Content */
+        .modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+        }
+
+        /* The Close Button */
+        .close {
+            color: #aaaaaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }     
+
+        textarea {
+            overflow-y: scroll;
+            height: 100px;
+            resize: none; /* Remove this if you want the user to resize the textarea */
+        }   
     </style>
 </head>
 
 <body>
-    <!-- <button id="screenshot"> SCREENSHOT </button>  -->     
+    <button id="myBtn"> SCREENSHOT </button>
+        <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+      <!-- Modal content -->
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <div id="durl">Some text in the Modal..</div>
+      </div>
+
+    </div>  
     <div>
         <img id="Logo" src="">      
     </div>
@@ -91,7 +147,7 @@ message = """
     <script src="assets/jquery.min.js"></script>
     <script src="assets/PLYLoader.js"></script>
     <script src="assets/dat.gui.min.js"></script> 
-     <script src="assets/CanvasRenderer.js"></script>    
+    <script src="assets/CanvasRenderer.js"></script>    
 
     <script>
         if (!Detector.webgl) Detector.addGetWebGLMessage();
@@ -196,10 +252,46 @@ message = """
             //
             // SCREENSHOT
             //
-            $("#screenshot").click(function() {
-                window.open( renderer.domElement.toDataURL("image/png"), "Final");
-                return false;
-            });            
+
+            // Get the modal
+            var modal = document.getElementById('myModal');
+
+            // Get the button that opens the modal
+            var btn = document.getElementById("myBtn");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal 
+            btn.onclick = function() {
+
+                var url = renderer.domElement.toDataURL("image/png");
+                var iH = "Following is the dataURL of your image. Please go <a href='https://codebeautify.org/base64-to-image-converter' target='_blank'>here</a> to convert it into an image.<br><br>";
+                var text = document.createElement("textarea");
+                text.value = url;
+
+                var durl = document.getElementById('durl');
+                durl.innerHTML = iH;
+                durl.append(text);
+
+
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+
+
+
             //
             // ORBIT CONTROLS
             //
--- random_ply_generator.py
+++ random_ply_generator.py
@@ -9,4 +9,13 @@ with open("cloud.ply", "a") as myfile:
 	myfile.write("ply\nformat ascii 1.0\nelement vertex "+str(vertex_count)+"\nproperty float x\nproperty float y\nproperty float z\nproperty uchar red\nproperty uchar green\nproperty uchar blue\nend_header\n")
 	
 	for x in range(vertex_count):
-		myfile.write(str(random.uniform(-0.5, 0.5)) + " " + str(random.uniform(-0.5, 0.5)) + " " + str(random.uniform(-0.5, 0.5)) + " " + str(random.randint(0,255)) + " " + str(random.randint(0,255)) + " " + str(random.randint(0,255)) + "\n")
+		x = random.uniform(-0.5, 0.5)
+		y = random.uniform(-0.5, 0.5)
+		z = random.uniform(-0.5, 0.5)
+		r = random.randint(0,255)
+		g = random.randint(0,255)
+		b = random.randint(0,255)
+
+		text = "{} {} {} {} {} {}\r\n".format(x, y, z, r, g, b)
+
+		myfile.write(text)
